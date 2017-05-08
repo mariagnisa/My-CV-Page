dd116b4b891fc2c8caf7ef63bbe58133d84d1303
Maria <maria.gnisa@gmail.com>
2017-05-08 17:02:42 +0200

    added js functions, content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,79 +1,72 @@
-// (function() {
-//
-//      'use strict';
-//
-//     // Feature Test
-//     if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
-//
-//         // Function to animate the scroll
-//         var smoothScroll = function (anchor, duration) {
-//
-//             // Calculate how far and how fast to scroll
-//             var startLocation = window.pageYOffset;
-//             var endLocation = anchor.offsetTop;
-//             var distance = endLocation - startLocation;
-//             var increments = distance/(duration/16);
-//             var stopAnimation;
-//
-//             // Scroll the page by an increment, and check if it's time to stop
-//             var animateScroll = function () {
-//                 window.scrollBy(0, increments);
-//                 stopAnimation();
-//             };
-//
-//             // If scrolling down
-//             if ( increments >= 0 ) {
-//                 // Stop animation when you reach the anchor OR the bottom of the page
-//                 stopAnimation = function () {
-//                     var travelled = window.pageYOffset;
-//                     if ( (travelled >= (endLocation - increments)) || ((window.innerHeight + travelled) >= document.body.offsetHeight) ) {
-//                         clearInterval(runAnimation);
-//                     }
-//                 };
-//             }
-//             // If scrolling up
-//             else {
-//                 // Stop animation when you reach the anchor OR the top of the page
-//                 stopAnimation = function () {
-//                     var travelled = window.pageYOffset;
-//                     if ( travelled <= (endLocation || 0) ) {
-//                         clearInterval(runAnimation);
-//                     }
-//                 };
-//             }
-//
-//             // Loop the animation function
-//             var runAnimation = setInterval(animateScroll, 16);
-//
-//         };
-//
-//         // Define smooth scroll links
-//         var scrollToggle = document.querySelectorAll('.scroll');
-//
-//         // For each smooth scroll link
-//         [].forEach.call(scrollToggle, function (toggle) {
-//
-//             // When the smooth scroll link is clicked
-//             toggle.addEventListener('click', function(e) {
-//
-//                 // Prevent the default link behavior
-//                 e.preventDefault();
-//
-//                 // Get anchor link and calculate distance from the top
-//                 var dataID = toggle.getAttribute('href');
-//                 var dataTarget = document.querySelector(dataID);
-//                 var dataSpeed = toggle.getAttribute('data-speed');
-//
-//                 // If the anchor exists
-//                 if (dataTarget) {
-//                     // Scroll to the anchor
-//                     smoothScroll(dataTarget, dataSpeed || 500);
-//                 }
-//
-//             }, false);
-//
-//         });
-//
-//     }
-//
-//  })();
+//Fetch navbar and content-wrapper
+let navbar = document.getElementById('nav');
+let content = document.getElementById('content');
+let sticky = false;
+
+let stickPoint = getDistance();
+
+function getDistance() {
+  let topDist = navbar.offsetTop;
+  return topDist;
+}
+
+// Fecth divs with content
+let education = document.getElementById('education');
+let skills = document.getElementById('skills');
+let experience = document.getElementById('experience');
+let contact = document.getElementById('contact');
+
+//Add divs with content to array
+const fadingArray = [];
+fadingArray.push(education);
+fadingArray.push(skills);
+
+const smallFading = [];
+smallFading.push(experience);
+smallFading.push(contact);
+
+//When page is scrolled, navbar gets sticky and divs content shows
+window.onscroll = function(e) {
+
+  let distance = getDistance() - window.pageYOffset;
+  let offset = window.pageYOffset;
+  if ( (distance <= 0) && !sticky) {
+    navbar.style.position = 'fixed';
+    navbar.style.top = '0px';
+    navbar.style.background = 'rgba(0, 102, 77, 0.9)';
+
+    sticky = true;
+  } else if (sticky && (offset <= stickPoint)){
+    navbar.style.position = 'static';
+    navbar.style.background = 'none';
+    sticky = false;
+  }
+
+  //Add fading to the big divs
+  for (let i = 0; i < fadingArray.length; i++) {
+    let distTop = fadingArray[i].offsetTop - fadingArray[i].offsetHeight - 200;
+
+    let divsDistance = distTop - window.pageYOffset;
+    let off = window.pageYOffset;
+
+    if (divsDistance <= 0) {
+      fadingArray[i].style.opacity = '1';
+    } else if (off <= distTop) {
+      fadingArray[i].style.opacity = '0';
+    }
+  }
+
+  //Add fading to the small divs
+  for (let i = 0; i < smallFading.length; i++) {
+    let distTop = smallFading[i].offsetTop - smallFading[i].offsetHeight - 400;
+
+    let divsDistance = distTop - window.pageYOffset;
+    let off = window.pageYOffset;
+
+    if (divsDistance <= 0) {
+      smallFading[i].style.opacity = '1';
+    } else if (off <= distTop) {
+      smallFading[i].style.opacity = '0';
+    }
+  }
+}
